09a680f0eceaacf1fdf00234aeb287e19d132c9a
SnackYori <1125201503@qq.com>
2024-01-27 13:43:29 +0800

    修复倒计时，摄像机限制

--- Assets/Scripts/CameraTrans.cs
+++ Assets/Scripts/CameraTrans.cs
@@ -9,9 +9,12 @@ public class CameraTrans : MonoBehaviour {
 	public GameObject Player1;  //声明玩家1
     public GameObject Player2;  //声明玩家2
 	private Vector3 offset;   //差值
-	private Transform playerTransform;  //声明玩家1的Transform组件 
-    private Transform playerTransform2;  //声明玩家2的Transform组件 
-	private Transform cameraTransform;  //声明相机的Transform组件 
+	private Transform playerTransform;  //玩家1组件
+    private Transform playerTransform2;  //玩家2组件
+	private Transform cameraTransform;  //相机组件
+	public Transform LDanchor;//左下锚点
+	public Transform RTanchor;//右上锚点
+
 	public float distance = 0;
     [Range(0,1)]public float transSpeed;
     public int maxRange;
@@ -26,12 +29,26 @@ public class CameraTrans : MonoBehaviour {
         playerTransform2 = Player2.GetComponent<Transform> (); //得到玩家2的Transform组件
 		cameraTransform = this.GetComponent<Transform> ();   //得到相机的Transform组件
 		offset = cameraTransform.position - (playerTransform.position+playerTransform2.position)/2;  //得到相机和玩家中间位置的差值
+		// Vector2 ld = theCamera.ViewprotToWorld (new Vector(0,0));
+		// Vector2 rt = theCamera.ViewprotToWorld (new Vector(1,1));
+
+		// Vector2 tmp = ld - rt;
+		// tmp/=2;
+	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		this.transform.position = (playerTransform.position+playerTransform2.position)/2 + offset;  //玩家中间位置加上差值赋值给相机的位置
+		this.transform.position = (playerTransform.position+playerTransform2.position)/2 + offset;//玩家中间位置加上差值赋值给相机的位置
 		Camera.main.orthographicSize = (-this.transform.position.z) + 5;
+
+		Debug.Log (LDanchor.position.x);
+
+		this.transform.position = 
+		new Vector3(Mathf.Clamp(this.transform.position.x, LDanchor.position.x, RTanchor.position.x), //相机限制
+		Mathf.Clamp(this.transform.position.y, LDanchor.position.y, RTanchor.position.y),-8);
+
+
 		CameraView();
 	}
 	void CameraView(){
